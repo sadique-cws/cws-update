88c7064e5728bb79f194d2bb55f5c43331587a9c
sadique-cws <sadique.hussain96@gmail.com>
2023-03-06 12:07:09 +0530

    generate course

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Payments;
+use App\Models\Paytm;
 use App\Models\StudentCourseDetails;
 use DateInterval;
 use DateTime;
@@ -21,13 +22,21 @@ class StudentController extends Controller
             $loan = $course->course->duration;
             $instalment = $totalLoan / $loan;
             $priceLoan = $instalment + $totalLoan * $interest / 100;
+
+            // dd($course->course->getPaidAmount());
             $fixedDateEveryMonth = 28;
             $start = new DateTime(date('Y-m-') . $fixedDateEveryMonth);
             for ($i = 1; $i <= $loan; $i++) {
                 $start->add(new DateInterval("P1M"));
                 $dateLoan = $start->format('d-M-Y');
                 echo "$dateLoan - " . number_format($priceLoan, 0) . "\n";
-                $priceLoan = $instalment;
+                $paymentData = [
+                    'student_id' => auth()->id,
+                    'amount' => $priceLoan,
+                ];
+               $payment =  Paytm::create($paymentData);
+                $priceLoan = $instalment; 
+                
             }
         }
     }
